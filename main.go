395b5d31087d55dde5c79d36755ecc84e4b760a0
John Slee <john.slee@siteminder.com>
2018-09-20 13:55:14 +1000

    optionally sleep before responding to requests

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"flag"
 	"io"
+	"log"
 	"net/http"
-  "flag"
-  "log"
+	"time"
 )
 
 func main() {
-  path    := flag.String("path","/","specify a path to respond to")
-  message := flag.String("message","hi m8","specify a message to be returned to clients")
-  listen  := flag.String("listen",":3000","[address]:port to bind to")
-  flag.Parse()
+	path := flag.String("path", "/", "specify a path to respond to")
+	message := flag.String("message", "hi m8", "specify a message to be returned to clients")
+	listen := flag.String("listen", ":3000", "[address]:port to bind to")
+	delay := flag.Duration("delay", 0, "sleep this duration before responding")
+	flag.Parse()
 	http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, *message)
-  })
+		time.Sleep(*delay)
+		io.WriteString(w, *message)
+	})
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
-
-
